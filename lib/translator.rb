db078d18774723d447ccaea22c4d205366d59cb6
Marcus Siegel <msiegel37@gmail.com>
2020-08-13 22:58:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
-# require modules here
+require 'yaml'
+require 'pry'
+def load_library(emoticon_file)
+  library = {
+    "japanese" => {},
+    "english" => {}
+  }
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons.each do |meaning, array|
+    english_emoticon = array[0]
+    japanese_emoticon = array[1]
+    library["japanese"][japanese_emoticon] = meaning
+    library["english"][english_emoticon] = array[1]
+    #binding.pry 
+ end
+library
+end 
 
-def load_library
-  # code goes here
-end
+def get_japanese_emoticon(emoticon_file, emoticon)
+  library = load_library(emoticon_file)
+  result = library['get_emoticon'][emoticon]
+  if result 
+    result
+    else 
+      "Sorry, that emoticon was not found"
+  end
+end 
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+def get_english_meaning(emoticon_file, emoticon)
+  library = load_library(emoticon_file)
+  result = library['get_meaning'][emoticon]
+  if result
+    result 
+    else
+      "Sorry, that emoticon was not found"
+ end
+end 
