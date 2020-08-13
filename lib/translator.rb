e6a1833f39a3977fa9f7fc3b4d6ef6283bae11ed
Marcus Siegel <msiegel37@gmail.com>
2020-08-13 23:30:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,8 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  library.keys.find do |
+  library.keys.find do |key| 
+    
 end 
 
 def get_english_meaning(file_path, emoticon)
