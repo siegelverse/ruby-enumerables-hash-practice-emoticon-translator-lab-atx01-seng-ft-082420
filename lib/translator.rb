e8177b862905547c0c4adc5e81de54bc35af6237
Marcus Siegel <msiegel37@gmail.com>
2020-08-13 23:33:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   emoticon = library.keys.find do |name| 
     library[name][:english] == emoticon 
+    if 
 end 
 
 def get_english_meaning(file_path, emoticon)
